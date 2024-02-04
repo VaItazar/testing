e2660a1eb29c4e2dce5570544dccb4099e77b9c3
Pavlicker <157319965+Pavlicker@users.noreply.github.com>
2024-02-04 18:16:09 +0100

    Update ckmachine.py

--- ckmachine.py
+++ ckmachine.py
@@ -1,19 +1,14 @@
 def main():
     amount = 50
 
-    while True:
+    while amount>0:
 
+        print(f'Amount Due: {amount}')
         inserted = int(input('insert coin: '))
 
         if inserted is 10 or inserted is 25 or inserted is 5:
             amount = amount - inserted
-            if amount <=0:
-                print('Change Owed:', (-1)*amount)
-                break
-            print(f'Amount Due: {amount}')
 
-        else:
-            print(f'Amount Due: {amount}')
-
+    print('Change Owed:', (-1)*amount)
 
 main()
